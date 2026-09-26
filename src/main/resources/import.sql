57988755adfc5ee362f35778736b0788474090d8
Franco <imnordi@gmail.com>
2026-09-25 22:44:22 -0300

    subo un nivel los imports para que funione en maria db

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT IGNORE INTO roles (id, name, description) VALUES ('ADMINISTRADOR', 'ADMINISTRADOR', 'Rol Administrador');
+INSERT IGNORE INTO roles (id, name, description) VALUES ('CURADOR', 'CURADOR', 'Rol Curador');
+INSERT IGNORE INTO roles (id, name, description) VALUES ('VISITANTE', 'VISITANTE', 'Rol Visitante');
+
